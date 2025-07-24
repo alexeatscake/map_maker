5e7a68877997443c2d38ae7880d54debf9ac2b1b
Alex Hickey <alexander.john.hickey@gmail.com>
2025-07-24 14:09:30 +0100

    :person_red_hair: Placeholder src

---
+++ src/map_maker/map_maker.py
@@ -0,0 +1,18 @@
+"""Dummy module to get started with the src structure."""
+
+
+def hello_map_maker():
+    """A simple hello function to test the module structure."""
+    return "Hello from Map Maker!"
+
+
+class MapMaker:
+    """A placeholder class for future map generation functionality."""
+
+    def __init__(self):
+        """Initialize the MapMaker."""
+        self.version = "0.1.0"
+
+    def generate_map(self):
+        """Placeholder method for map generation."""
+        return "Map generation not yet implemented!"
